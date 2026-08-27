92affd4c0bc92061153d5ad53592a69d4a7a15f7
Gavin Corkins <gavincorkins@gmail.com>
2026-08-27 14:06:20 -0400

    Fixed code to instead of passing a copy of game_state through, sends the exact address.

--- PigDice.cpp
+++ PigDice.cpp
@@ -2,7 +2,6 @@
 
 // Build your solution starting from this code.
 using namespace std;
-
 struct GameState {
     char choice;
     int turn_count = 0;
@@ -11,22 +10,24 @@ struct GameState {
     bool game_over = false;
     bool turn_over = false;
 };
+void takeTurn(GameState &gs);
+
 void display_rules() {
     cout << "Let's Play PIG Dice!\n"
             "\n"
             "* See how many turns it takes you to get to 20 points.\n"
             "* Turn ends when you hold or roll a 1.\n"
             "* If you roll a 1, you lose all points for the turn.\n"
-            "* If you hold, you bank all points for the turn to the game score."
+            "* If you hold, you bank all points for the turn to the game score.\n"
     << endl;
 }
 
-void playGame(GameState my_game) {
-
+void playGame(GameState &gs) {
+    takeTurn(gs);
 }
 
-void takeTurn(GameState my_game) {
-    cout << "TURN " << my_game.turn_count << endl;
+void takeTurn(GameState &gs) {
+    cout << "TURN " << gs.turn_count << endl;
 }
 
 int roll() {
@@ -37,6 +38,7 @@ int main() {
     srand(time(0));
     GameState my_game; // instantiate a GameState object
     display_rules(); // call the display_rules function
+    playGame(my_game);
     //play_game(my_game); // call the play_game function and pass the GameState object
     return 0;
 }
