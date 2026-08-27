b4d0682f983d927bca2501fb02adaeec5fce7eb0
GavinRC <gavincorkins@gmail.com>
2026-08-27 13:49:25 -0400

    Initial setup for program.

--- PigDice.cpp
+++ PigDice.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 // Build your solution starting from this code.
+using namespace std;
 
 struct GameState {
     char choice;
@@ -10,10 +11,33 @@ struct GameState {
     bool game_over = false;
     bool turn_over = false;
 };
+void display_rules() {
+    cout << "Let's Play PIG Dice!\n"
+            "\n"
+            "* See how many turns it takes you to get to 20 points.\n"
+            "* Turn ends when you hold or roll a 1.\n"
+            "* If you roll a 1, you lose all points for the turn.\n"
+            "* If you hold, you bank all points for the turn to the game score."
+    << endl;
+}
+
+void playGame(GameState my_game) {
+
+}
+
+void takeTurn(GameState my_game) {
+    cout << "TURN " << my_game.turn_count << endl;
+}
+
+int roll() {
+    return (rand() % 6 + 1);
+}
 
 int main() {
+    srand(time(0));
     GameState my_game; // instantiate a GameState object
     display_rules(); // call the display_rules function
     play_game(my_game); // call the play_game function and pass the GameState object
     return 0;
 }
+
